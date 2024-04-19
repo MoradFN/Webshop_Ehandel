b2638c16927a43b251af2580baaeb5039e0a179a
Morad Tumah <morad90@live.se>
2024-04-19 19:12:52 +0200

    hämta och ta ut data(DB)

--- server/server.js
+++ server/server.js
@@ -1,1 +1,46 @@
-console.log("server.js");
+const mongodb = require("mongodb");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const url = "mongodb://localhost:27017";
+  const MongoClient = new mongodb.MongoClient(url);
+
+  MongoClient.connect()
+    .then(() => {
+      console.log("connected to mongodb"); // så fort du upprättar/startar Docker Imgen får du svaret "connected to mongodb".
+
+      let dataBase = MongoClient.db("testDB");
+      let collection = dataBase.collection("test");
+
+      // lägga in data i databasen
+      return collection
+        .insertMany([
+          { a: 1 },
+          { a: 2 },
+          { a: 3, name: "Test product", price: 100 },
+        ])
+        .then(() => {
+          console.log("Data inserted");
+
+          // och hämta ut data från databasen.
+          return collection
+            .find({ name: "Test product" })
+            .toArray()
+            .then((results) => {
+              console.log("Found: ", results);
+              response.json(results);
+            });
+        });
+    })
+    .finally(() => {
+      MongoClient.close();
+    });
+});
+
+app.listen(3000);
+
+/*
+
+  */
